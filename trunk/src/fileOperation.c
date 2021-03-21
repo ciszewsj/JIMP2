7b366b9f928eec9b182d8c1c5143c2b92779a6d3
Aliaksandra <01155188@pw.edu.pl>
2021-03-21 18:01:31 +0300

    dodalam wczytywanie danych z pliku

--- trunk/src/fileOperation.c
+++ trunk/src/fileOperation.c
@@ -2,24 +2,28 @@
 #include "tableOperation.h"
 
 table* readFromFile(FILE* file)
-{
-	table* gameTable;
-	int i,j;
-	char c;
-	if (file == NULL)
-	{
-		gameTable = initTable(3,3);
-		for (i=0; i<3; i++)
-		{
-			if (i%2 == 0)
-				c='0';
-			else
-				c='1';
-			for (j=0; j<3; j++)
-			{
-				gameTable->board[i][j] = c;
-			}
-		}
+{	FILE *in= fopen( file, "r" );
+    if( in == NULL ) {
+    fprintf( stderr, "Plik o podanej nazwie nie istnieje. Należy podać nazwę istniejęcego pliku  wejsciowego" );
+    return EXIT_FAILURE;
+}
+
+	int zmienne[2];
+	for (int i = 0; i < 2; i++) {
+		fscanf(in, "%d", &zmienne[i]);
 	}
+	table *gameTable;
+	gameTable = initTable(zmienne[0],zmienne[1]);
+	int wartosci[zmienne[0]*zmienne[1]]
+	
+	for (int i = 0; i < zmienne[0] * zmienne[1]; i++){
+		fscanf(in, "%d", &wartosci[i]) ;
+	}
+	int k = 0;
+	for (int i = 0; i < zmienne[0]; i++){
+		for (int j = 0; j < zmienne[1]; j++)
+			gameTable->board[i][j] = wartosci[k++];
+	}
+
 	return gameTable;
 }
