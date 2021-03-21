ae4370d28c703d2917b282939d14104aee46264c
Jakub Ciszewski <01159203@pw.edu.pl>
2021-03-21 17:58:09 +0100

    v0.3.7 Dodano opis fileOperation

--- trunk/src/fileOperation.c
+++ trunk/src/fileOperation.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "tableOperation.h"
 
-table* readFromFile(FILE* file)
-{	FILE *in= fopen( file, "r" );
-    if( in == NULL ) {
-    fprintf( stderr, "Plik o podanej nazwie nie istnieje. Należy podać nazwę istniejęcego pliku  wejsciowego" );
-    return EXIT_FAILURE;
+table* readFromFile(FILE* file) // tu przekazanay jest juz wczytany plik do odczytania
+
+	//przenioslbym wszystkie deklaracje na gore
+{	FILE *in= fopen( file, "r" ); // ta czesc niepotrzeba
+    if( in == NULL ) {//
+    fprintf( stderr, "Plik o podanej nazwie nie istnieje. Należy podać nazwę istniejęcego pliku  wejsciowego" );//
+    return EXIT_FAILURE;// do tad
 }
 
 	int zmienne[2];
@@ -14,13 +16,15 @@ table* readFromFile(FILE* file)
 	}
 	table *gameTable;
 	gameTable = initTable(zmienne[0],zmienne[1]);
-	int wartosci[zmienne[0]*zmienne[1]]
+	int wartosci[zmienne[0]*zmienne[1]] // nie mozna tq zrobic . trzeba uzyc malloc lepiej calloc :(
 	
-	for (int i = 0; i < zmienne[0] * zmienne[1]; i++){
-		fscanf(in, "%d", &wartosci[i]) ;
+	for (int i = 0; i < zmienne[0] * zmienne[1]; i++)
+	{
+		fscanf(in, "%d", &wartosci[i]) ; // tu zamiast %d powinnien byc %c wartsosc[i] moze zostac. + dodac sprawdzenie czy nie za malo, (nie za duzo) zwrocic NULL
 	}
 	int k = 0;
-	for (int i = 0; i < zmienne[0]; i++){
+	for (int i = 0; i < zmienne[0]; i++) 
+	{
 		for (int j = 0; j < zmienne[1]; j++)
 			gameTable->board[i][j] = wartosci[k++];
 	}
